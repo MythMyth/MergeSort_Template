be754dffdeff69f2eecd66809506bf714b3571b2
MythMyth <windy10091996@gmail.com>
2023-05-28 16:29:50 +0700

    Add implementation

---
+++ MegerSortTemplate.h
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T, typename cmp = less<T&>>
+class MergeSort {
+public:
+    typedef void (*MergeCallback)(const size_t, const size_t, const size_t, const size_t, const size_t, const size_t, const vector<T>&);
+    MergeSort() : arr(new vector<T>()) {
+        before_merge_first_segment_item = MergeSort::DoNothing;
+        before_merge_second_segment_item = MergeSort::DoNothing;
+    }
+    explicit MergeSort(const vector<T> &orgArr, MergeCallback firstSegItemMerge = MergeSort::DoNothing, MergeCallback secondSegItemMerge = MergeSort::DoNothing) : arr(new vector<T>()) {
+        size_t len = arr->size();
+        for(size_t i = 0U; i < len; ++i) {
+            arr->push_back(orgArr[i]);
+        }
+        before_merge_first_segment_item = firstSegItemMerge;
+        before_merge_second_segment_item = secondSegItemMerge;
+    }
+
+    MergeSort(const MergeSort& another) = delete ;
+    MergeSort(MergeSort&& another) = delete ;
+    MergeSort& operator= (const MergeSort& another) = delete ;
+    MergeSort& operator= (MergeSort&& another) = delete ;
+
+    virtual ~MergeSort() {
+        if(arr != NULL) {
+            delete arr;
+        }
+    }
+
+    void Sort() {
+        if(arr->size() > 0) {
+            _SortUtil(0U, arr->size() - 1U);
+        }
+    }
+
+    void SetFirstSegItemMergeCallback(MergeCallback func) { before_merge_first_segment_item = func; }
+    void SetSecSegItemMergeCallback(MergeCallback func) { before_merge_second_segment_item = func; }
+    void push_back(const T& item) { arr->push_back(item); }
+    T& operator[] (const size_t index) { return (*arr)[index]; }
+    T& Get(const size_t index) { return (*arr)[index]; }
+    size_t size() const { return arr->size();}
+
+private:
+    vector<T> *arr;
+    MergeCallback before_merge_second_segment_item;
+    MergeCallback before_merge_first_segment_item;
+    cmp compare;
+
+    static void DoNothing(const size_t p1, const size_t p2, const size_t p3, const size_t p4, const size_t p5, const size_t p6, const vector<T>& arr) {}
+
+    void _SortUtil(size_t start, size_t end) {
+        if(start == end) {
+            return;
+        }
+        size_t endOfFirst = start + (end - start) / 2U;
+        size_t startOfSec = endOfFirst + 1U;
+        _SortUtil(start, endOfFirst);
+        _SortUtil(startOfSec, end);
+        vector<T> temp;
+        size_t fi = start, si = startOfSec, ti = 0U;
+        while(fi <= endOfFirst || si <= end) {
+            if(si > end || (fi <= endOfFirst && compare((*arr)[fi], (*arr)[si]) == true)) {
+                before_merge_first_segment_item(fi, si, start, endOfFirst, startOfSec, end, *arr);
+                temp.push_back((*arr)[fi]);
+                fi++;
+            } else {
+                before_merge_second_segment_item(fi, si, start, endOfFirst, startOfSec, end, *arr);
+                temp.push_back((*arr)[si]);
+                si++;
+            }
+        }
+        size_t tempLen = temp.size();
+        for(size_t i = 0; i < tempLen; ++i) {
+            (*arr)[i+start] = temp[i];
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
